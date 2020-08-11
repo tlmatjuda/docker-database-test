b34b5738ed2c45cff8892964b43ef08eeaed3d1b
Thabo Matjuda <thabo@anylytical.co.za>
2020-08-11 18:08:38 +0200

    Initial push of the repo ... YAY!

---
+++ src/main/java/za/co/anylytical/showcase/dockerdatabasetest/DockerDatabaseTestApplication.java
@@ -0,0 +1,13 @@
+package za.co.anylytical.showcase.dockerdatabasetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDatabaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDatabaseTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/anylytical/showcase/dockerdatabasetest/DockerDatabaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.anylytical.showcase.dockerdatabasetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDatabaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
